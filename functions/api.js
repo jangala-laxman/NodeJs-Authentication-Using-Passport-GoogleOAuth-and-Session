b6a5092ba0564eeb4279f929e98cd2645a17bfe0
Sai Sri Laxman Jangala <74813943+jangala-laxman@users.noreply.github.com>
2023-04-11 17:53:43 +0530

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -3,9 +3,9 @@ const app= express()
 const router = express.Router()
 const serverless = require('serverless-http')
 
-// router.get("/", (req, res)=>{
-//     res.redirect("/home")
-// })
+router.get("/", (req, res)=>{
+    res.redirect("/home")
+})
 
 app.use('/.netlify/functions/api', router)
 module.exports.handler = serverless(app)
