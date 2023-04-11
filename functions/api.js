c3162fdad68e4c3d9af74723263288cdf058313f
Sai Sri Laxman Jangala <jangalasaisrilaxman9@gmail.com>
2023-04-11 17:00:57 +0530

    seconf commit

--- functions/api.js
+++ functions/api.js
@@ -4,8 +4,9 @@ const router = express.Router()
 const serverless = require('serverless-http')
 
 router.get("/", (req, res)=>{
-    res.send("home")
+    res.render("home")
 })
 
 app.use('/.netlify/functions/api', router)
 module.exports.handler = serverless(app)
+ 
